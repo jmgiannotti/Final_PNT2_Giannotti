15785a2b0232c9429cb3d2da0d27d072a8039382
jmgiannotti <juanmartingiannotti@gmail.com>
2022-12-13 20:43:02 -0300

    entrega

---
+++ src/componentes/ApiRestFul.vue
@@ -0,0 +1,99 @@
+<template>
+
+  <section>
+    <div class="jumbotron">
+      <h2>Conversor a dolares</h2>
+      <br>
+      <p>Ingrese Monto $<input name="cantidad" v-model="cantidad" type="number"></p>
+      <p>Valor del dolar en $<input name="valorDado" v-model="valorDado" value="valorDado" type="number"> - Actualizacion <input type="checkbox" v-model="checked" @input="chequeado()"  ></p><p v-if="checked"> {{dateTime.fecha}}</p> 
+      <p> Valor convertido USD {{ resultado }} </p>
+
+    </div>      
+  </section>
+
+</template>
+
+<script>
+
+  export default  {
+    name: 'src-componentes-api-rest-ful',
+    props: [],
+    mounted () {
+      //this.getDolarBlue()
+      this.setDateTime()
+    },
+    data () {
+      const date = new Date()
+      return {
+        checked: false,
+        mostrar: false,
+        cantidad: 1,
+        valorDado: 1,
+        url: 'https://www.dolarsi.com/api/api.php?type=valoresprincipales',
+        dolares : [],
+        dolarBlue: 1,
+        dateTime: {
+          hours: date.getHours(),
+          minutes: date.getMinutes(),
+          seconds: date.getSeconds(),
+        },
+      }
+    },
+    methods: {
+      /* ---------------------------------- */
+      /*           API REST : GET           */
+      /* ---------------------------------- */
+      async getDolarBlue() {
+        try {
+          
+          let { data : dolares } = await this.axios(this.url)
+         
+          this.dolares = dolares
+          this.dolarBlue =  parseFloat(dolares[1].casa.venta)
+          this.valorDado = this.dolarBlue
+          this.mostrar = true
+        }
+        catch(error) {
+          console.error('Error en getDolarBlue', error.message)
+        }
+      },
+      chequeado(){
+        if(this.checked){
+            this.getDolarBlue()
+            this.setDateTime()
+        }
+      },
+      setDateTime() {
+        const date = new Date();
+        this.dateTime = {
+          fecha: date.toLocaleString(),
+          
+        };
+      },
+                  
+    },
+    computed: {
+        resultado(){
+          return this.cantidad / this.valorDado
+        }
+    }
+}
+
+
+</script>
+
+<style scoped lang="css">
+  .jumbotron {
+    background-color: navy;
+    color: white;
+  }
+
+  hr {
+    background-color: #bbb;
+  }
+
+  pre {
+    color: white;
+  }
+  
+  </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+
+/* -------------- BOOTSTRAP --------------- */
+//https://getbootstrap.com/docs/4.6/getting-started/introduction/
+//Instalación: npm i bootstrap@4 jquery popper.js
+import './bootstrap'
+
+
+/* -------------- AXIOS --------------- */
+//https://www.npmjs.com/package/axios
+//https://www.npmjs.com/package/vue-axios
+//Instalación: npm i axios vue-axios@3.4.1
+import './axios'
+
+
 Vue.config.productionTip = false
 
 new Vue({
